10a8c47a9ce2fef7f40dabb487ebfea59efd8345
Ben Ilegbodu <ben.ilegbodu@eventbrite.com>
2016-11-30 16:22:27 -0800

    Add rules from eslint-plugin-import

--- packages/eslint-config-eventbrite/rules/es6.js
+++ packages/eslint-config-eventbrite/rules/es6.js
@@ -8,6 +8,12 @@ module.exports = {
         ecmaFeatures: {
         }
     },
+    plugins: [
+        'import'
+    ],
+    extends: [
+        'plugin:import/errors'
+    ],
     rules: {
         // require no braces where they can be omitted
         // http://eslint.org/docs/rules/arrow-body-style
@@ -21,13 +27,53 @@ module.exports = {
         // http://eslint.org/docs/rules/arrow-spacing
         'arrow-spacing': 'error',
 
+        // disallow any invalid exports, i.e. re-export of the same name
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/export.md
+        'import/export': 'error',
+
+        // ensure all imports appear before other statements
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
+        'import/first': 'error',
+
+        // enforce a newline after import statements
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
+        'import/newline-after-import': 'error',
+
+        // disallow import of modules using absolute paths
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-absolute-path.md
+        'import/no-absolute-path': 'error',
+
+        // disallow require() calls with expressions
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
+        'import/no-dynamic-require': 'error',
+
+        // disallow repeated import of the same module in multiple places
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
+        'import/no-duplicates': 'error',
+
+        // disallow the use of extraneous packages
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+        'import/no-extraneous-dependencies': 'error',
+
+        // disallow the use of mutable exports with var or let
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-mutable-exports.md
+        'import/no-mutable-exports': 'error',
+
+        // ensure imports point to a file/module that can be resolved
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
+        'import/no-unresolved': 'error',
+
+        // disallow Webpack loader syntax in imports
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-webpack-loader-syntax.md
+        'import/no-webpack-loader-syntax': 'error',
+
+        // disallow potentially ambiguous parse goal (script vs. module)
+        // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/unambiguous.md
+        'import/unambiguous': 'error',
+
         // allow arrow functions where they could be confused with comparisons
         // http://eslint.org/docs/rules/no-confusing-arrow
         'no-confusing-arrow': 'off',
-
-        // disallow duplicate module imports & exports
-        // http://eslint.org/docs/rules/no-duplicate-imports
-        'no-duplicate-imports': ['error', {includeExports: true}],
 
         // disallow unnecessary computed property keys in object literals
         // http://eslint.org/docs/rules/no-useless-computed-key
