0839a89756526aa63155e2eb6548044b70f02d46
Nam-Chi Van <namchi@eventbrite.com>
2017-06-20 16:21:50 -0700

    Add commas section to general styleguide (#49)
    * Add commas section to general styleguide
    
    * Update docs
    
    * remove period
    
    * Update eslint rule

--- packages/eslint-config-eventbrite/rules/errors.js
+++ packages/eslint-config-eventbrite/rules/errors.js
@@ -1,11 +1,13 @@
 // The rules ultimately override any rules defined in legacy/rules/errors.js
 module.exports = {
     rules: {
-        // allows (but does not require) trailing commas when the
-        // last element or property is in a different line than the
-        // closing ] or } and disallows trailing commas when the last
-        // element or property is on the same line as the closing ] or }
         // http://eslint.org/docs/rules/comma-dangle
-        'comma-dangle': ['error', 'only-multiline']
+        'comma-dangle': ['error', {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "never",
+            "exports": "never",
+            "functions": "ignore"
+        }]
     }
 };
