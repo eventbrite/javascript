4070f6c5ed563afd6a75ec72484983f0762686af
Ben Ilegbodu <ben@benmvp.com>
2018-05-22 12:21:20 -0700

    Move no-invalid-this into esnext rules

--- packages/eslint-config-eventbrite/rules/best-practices.js
+++ packages/eslint-config-eventbrite/rules/best-practices.js
@@ -4,10 +4,5 @@ module.exports = {
         // disallow modifying properties of parameters
         // http://eslint.org/docs/rules/no-param-reassign
         'no-param-reassign': ['error', {props: true}],
-
-        // allow `this` keywords outside of classes or class-like objects
-        // so that `babel/no-invalid-this` can disallow (see es6 rules)
-        // http://eslint.org/docs/rules/no-invalid-this
-        'no-invalid-this': 'off',
     },
 };
