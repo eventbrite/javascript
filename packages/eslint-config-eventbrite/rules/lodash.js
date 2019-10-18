a48a86534f729e55c22cb693036a2eb21935e3bd
rchapman-eb <rchapman@eventbrite.com>
2019-10-17 17:31:14 -0700

    Remove accidentally added files

--- packages/eslint-config-eventbrite/rules/lodash.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rules: {
-        // Enforce atomic imports of lodash methods to improve bundle size
-        // https://github.com/wix/eslint-plugin-lodash/blob/master/docs/rules/import-scope.md
-        'import-scope': 'method',
-    },
-};
